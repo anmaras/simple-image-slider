de4e1cb58395963fccc7a2fb625091382a79a58d
Antonis <marasantonis@gmail.com>
2022-06-15 18:58:54 +0300

    image slider update

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const leftButton = document.querySelector(".btn-left");
 const rightButton = document.querySelector(".btn-right");
-const item = document.querySelector(".item1");
+const container = document.querySelector(".container");
+const slider = document.querySelector(".slider");
+const imgsList = document.querySelectorAll("img");
+let counter = 0;
 
-leftButton.addEventListener("click", moveDivLeft);
-rightButton.addEventListener("click", moveDivRight);
+let size = imgsList[0].clientWidth;
 
-function moveDivLeft() {}
-function moveDivRight() {}
+const newSize = setTimeout(() => {
+  return imgsList[0].clientWidth;
+}, 0);
+
+rightButton.addEventListener("click", () => {
+  slider.style.transition = "transform 0.8s ease-in-out";
+
+  if (counter > imgsList.length - 2) {
+    counter = -1;
+    slider.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  counter++;
+  slider.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+leftButton.addEventListener("click", () => {
+  slider.style.transition = "transform 0.8s ease-in-out";
+
+  if (counter <= 0) {
+    counter = imgsList.length - counter;
+    slider.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  counter--;
+  slider.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+console.log(size);
