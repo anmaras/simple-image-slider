1005e52a43e0f0f31370ce0e4ad8f4a64e4dd04a
Antonis <marasantonis@gmail.com>
2022-06-15 12:24:27 +0300

    image slider update

---
+++ script.js
@@ -0,0 +1,9 @@
+const leftButton = document.querySelector(".btn-left");
+const rightButton = document.querySelector(".btn-right");
+const item = document.querySelector(".item1");
+
+leftButton.addEventListener("click", moveDivLeft);
+rightButton.addEventListener("click", moveDivRight);
+
+function moveDivLeft() {}
+function moveDivRight() {}
