8836a52c42200451e78ee864617c10e443b12b3e
Antonis <marasantonis@gmail.com>
2022-06-15 20:30:05 +0300

    image slider update

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const rightButton = document.querySelector(".btn-right");
 const container = document.querySelector(".container");
 const slider = document.querySelector(".slider");
 const imgsList = document.querySelectorAll("img");
+const img = document.querySelector("img");
+const radioWrapper = document.querySelector(".radio-wrapper");
 let counter = 0;
 
 let size = imgsList[0].clientWidth;
 
-const newSize = setTimeout(() => {
-  return imgsList[0].clientWidth;
-}, 0);
-
 rightButton.addEventListener("click", () => {
-  slider.style.transition = "transform 0.8s ease-in-out";
+  slider.style.transition = "transform 1s ease-in-out";
 
   if (counter > imgsList.length - 2) {
     counter = -1;
@@ -23,7 +21,7 @@ rightButton.addEventListener("click", () => {
 });
 
 leftButton.addEventListener("click", () => {
-  slider.style.transition = "transform 0.8s ease-in-out";
+  slider.style.transition = "transform 1s ease-in-out";
 
   if (counter <= 0) {
     counter = imgsList.length - counter;
@@ -33,4 +31,26 @@ leftButton.addEventListener("click", () => {
   slider.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-console.log(size);
+function renderRadioMarkUp() {
+  imgsList.forEach(createRadioMarkUp);
+}
+
+function createRadioMarkUp() {
+  const radioBtn = document.createElement("input");
+  radioBtn.type = "radio";
+  radioBtn.dataset.id = counter++;
+  return radioWrapper.append(radioBtn);
+}
+
+renderRadioMarkUp();
+
+// setInterval(() => {
+//   slider.style.transition = "transform 0.8s ease-in-out";
+
+//   if (counter > imgsList.length - 2) {
+//     counter = -1;
+//     slider.style.transform = "translateX(" + -size * counter + "px)";
+//   }
+//   counter++;
+//   slider.style.transform = "translateX(" + -size * counter + "px)";
+// }, 3000);
