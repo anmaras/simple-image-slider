f6dbcc30e85cd94c8bd105c5f16e987d17bf8e42
Antonis <marasantonis@gmail.com>
2022-06-16 13:02:12 +0300

    add radio button functionality

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ const imgsList = document.querySelectorAll("img");
 const img = document.querySelector("img");
 const radioWrapper = document.querySelector(".radio-wrapper");
 let counter = 0;
-
+let radioId = 0;
 let size = imgsList[0].clientWidth;
 
-rightButton.addEventListener("click", () => {
-  slider.style.transition = "transform 1s ease-in-out";
+function createRadioMarkUp() {
+  const radioBtn = document.createElement("input");
+  radioBtn.type = "radio";
+  radioBtn.dataset.id = radioId++;
+  radioBtn.name = "img button";
+  return radioWrapper.append(radioBtn);
+}
 
+function renderRadioMarkUp() {
+  imgsList.forEach(createRadioMarkUp);
+}
+
+renderRadioMarkUp();
+
+radioWrapper.childNodes.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    slider.style.transition = "transform 1s ease-in-out";
+
+    slider.style.transform = "translateX(" + -size * this.dataset.id + "px)";
+  })
+);
+
+rightButton.addEventListener("click", () => {
   if (counter > imgsList.length - 2) {
     counter = -1;
     slider.style.transform = "translateX(" + -size * counter + "px)";
   }
+  slider.style.transition = "transform 1s ease-in-out";
   counter++;
   slider.style.transform = "translateX(" + -size * counter + "px)";
+  showRadioActive(counter);
 });
 
 leftButton.addEventListener("click", () => {
@@ -29,23 +51,41 @@ leftButton.addEventListener("click", () => {
   }
   counter--;
   slider.style.transform = "translateX(" + -size * counter + "px)";
+  showRadioActive(counter);
 });
 
-function renderRadioMarkUp() {
-  imgsList.forEach(createRadioMarkUp);
+function showRadioActive(counter) {
+  const btn = radioWrapper.childNodes;
+  const btnNext = btn[counter].nextSibling ?? btn[0];
+  const btnPrevious = btn[counter].previousSibling ?? btn[imgsList.length - 1];
+  const checkIfPreviousIsActive = btnPrevious.classList.contains("checked");
+  const checkIfNextIsActive = btnNext.classList.contains("checked");
+
+  const array = [...btn];
+
+  // if (checkIfPreviousIsActive || checkIfNextIsActive) {
+  //   btnPrevious.classList.remove("checked");
+  //   btnNext.classList.remove("checked");
+  // }
+
+  btn.forEach((btn) => btn.classList.remove("checked"));
+
+  btn[counter].classList.add("checked");
 }
 
-function createRadioMarkUp() {
-  const radioBtn = document.createElement("input");
-  radioBtn.type = "radio";
-  radioBtn.dataset.id = counter++;
-  radioBtn.name = "img";
-  return radioWrapper.append(radioBtn);
-}
+window.addEventListener("load", () => {
+  radioWrapper.children[counter].classList.add("checked");
+});
 
-renderRadioMarkUp();
+radioWrapper.childNodes.forEach((node) =>
+  node.addEventListener("click", () => {
+    showRadioActive(node.dataset.id);
 
-// setInterval(() => {
+    // clearInterval(timedSliding);
+  })
+);
+
+// const timedSliding = setInterval(() => {
 //   slider.style.transition = "transform 0.8s ease-in-out";
 
 //   if (counter > imgsList.length - 2) {
@@ -54,4 +94,5 @@ renderRadioMarkUp();
 //   }
 //   counter++;
 //   slider.style.transform = "translateX(" + -size * counter + "px)";
+//   showRadioActive(counter);
 // }, 3000);
